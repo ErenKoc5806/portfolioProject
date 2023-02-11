48da47b3991916ce0b3c73c6cff735bcc10504ca
ErenKoc5806 <mernkoc@gmail.com>
2023-02-11 22:29:51 +0300

    update

--- capstone-project/src/App.js
+++ capstone-project/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. 31
+          Edit <code>src/App.js</code> and save to reload. 32
         </p>
         <a
           className="App-link"
